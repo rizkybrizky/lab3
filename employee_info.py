d05d902f616a5baf7940cb4590e725eff5b9358a
rizkybrizky <muhammadhariezzz03.24@ichat.sp.edu.sg>
2025-11-27 16:49:02 +0800

    completed calculate average salary function

--- employee_info.py
+++ employee_info.py
@@ -22,10 +22,12 @@ def calculate_average_salary():
     total = 0
     average = 0
 
-    #add your implementation to calculate here
-
+    for item in employee_data:
+        total += int(item['salary'])
+    average=total/(len(employee_data))
 
     return average
+
 
 def get_employees_by_dept(department):
     result = []
